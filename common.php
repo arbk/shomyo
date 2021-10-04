3719baec99f321e48a13f65a3bdacd5d4056c6fd
arbk <arbk@aruo.net>
2021-10-04 11:10:22 +0900

    shomyo v2.17.2

--- common.php
+++ common.php
@@ -2,8 +2,8 @@
 
 $f3 = require(__DIR__.'/libs/f3/base.php');
 
-$f3->set('DEBUG',0);
-$f3->set('version','2.17.1');
+$f3->set('DEBUG', 0);
+$f3->set('version', '2.17.2');
 $f3->set('AUTOLOAD', __DIR__ . '/;libs/f3/;libs/;libs/JShrink/;libs/WideImage/;daos/;libs/twitteroauth/;libs/FeedWriter/');
 $f3->set('cache', __DIR__ . '/data/cache');
 $f3->set('BASEDIR', __DIR__);
@@ -20,10 +20,10 @@ if (file_exists('config.ini')) {
 
 // overwrite config with ENV variables
 $env_prefix = $f3->get('env_prefix');
-foreach($f3->get('ENV') as $key => $value) {
-  if(strncasecmp($key,$env_prefix,strlen($env_prefix)) == 0) {
-    $f3->set(strtolower(substr($key,strlen($env_prefix))),$value);
-  }
+foreach ($f3->get('ENV') as $key => $value) {
+    if (strncasecmp($key, $env_prefix, strlen($env_prefix)) == 0) {
+        $f3->set(strtolower(substr($key, strlen($env_prefix))), $value);
+    }
 }
 
 // error log
@@ -33,21 +33,21 @@ ini_set("error_log", $f3->get('LOGDIR') . 'error.log');
 // init logger
 $f3->set(
     'logger',
-    new \helpers\Logger( $f3->get('LOGDIR') . 'default.log', $f3->get('logger_level') )
+    new \helpers\Logger($f3->get('LOGDIR') . 'default.log', $f3->get('logger_level'))
 );
 
 // init error handling
-$f3->set('ONERROR',
-    function($f3) {
+$f3->set(
+    'ONERROR',
+    function ($f3) {
         $trace = $f3->get('ERROR.trace');
         $tracestr = "\n";
-        if( is_array($trace) ){
-          foreach($trace as $entry) {
-              $tracestr = $tracestr . $entry['file'] . ':' . $entry['line'] . "\n";
-          }
-        }
-        else{
-          $tracestr = $tracestr . $trace . "\n";
+        if (is_array($trace)) {
+            foreach ($trace as $entry) {
+                $tracestr = $tracestr . $entry['file'] . ':' . $entry['line'] . "\n";
+            }
+        } else {
+            $tracestr = $tracestr . $trace . "\n";
         }
 
         \F3::get('logger')->log($f3->get('ERROR.text') . $tracestr, \ERROR);
@@ -62,5 +62,5 @@ $f3->set('ONERROR',
 );
 
 if (\F3::get('DEBUG') != 0) {
-    ini_set('display_errors',0);
+    ini_set('display_errors', 0);
 }
